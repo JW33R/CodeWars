1e1290cce4057d1d6fe1fe636ea3862376ad10a9
Tyler <108450341+JW33R@users.noreply.github.com>
2025-09-21 19:23:58 -0400

    Add files via upload

--- 7kyu/PizzaPayments/CodeWarsPizzaPayment.py
+++ 7kyu/PizzaPayments/CodeWarsPizzaPayment.py
@@ -1,9 +1,12 @@
-import math
-def michealPays(cost):
+
+def michaelPays(cost):
     if cost < 5:
-        return cost
-    elif cost > 5:
+        return round(cost, 2)
+    elif cost >= 5 and cost <= 30:
         result = cost * (1/3)
         result = cost - result
         return round(result, 2)
-print(michealPays(40))
+    else:
+        result = cost - 10
+        return round(result, 2)
+print(michaelPays(40))
