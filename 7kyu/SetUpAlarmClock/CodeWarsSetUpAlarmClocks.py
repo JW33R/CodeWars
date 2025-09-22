28b10e4c1974a6c0b4a4ec88cc7093e49e2704a4
Tyler <108450341+JW33R@users.noreply.github.com>
2025-09-22 13:48:29 -0400

    Update CodeWarsSetUpAlarmClocks.py

--- 7kyu/SetUpAlarmClock/CodeWarsSetUpAlarmClocks.py
+++ 7kyu/SetUpAlarmClock/CodeWarsSetUpAlarmClocks.py
@@ -1,6 +1,9 @@
+#The setTheAlarmsUp function uses the datetime and timedelta module. It first takes the time parameter and changes the string into a datetime object, then takes that object and adds 5 minutes for every time that the user wants with n.
+#Then after that, the object is put back into a string and returned.
 from datetime import datetime, timedelta
 def setTheAlarmsUp(time, n):
     timeObj = datetime.strptime(time, "%H:%M")
     addTime = [(timeObj + timedelta(minutes=5*_)).strftime("%H:%M") for _ in range(n)]
     return addTime
+
 print(setTheAlarmsUp("8:50", 2))
