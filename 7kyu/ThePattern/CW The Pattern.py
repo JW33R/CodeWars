f1b57bfd6c9ea45bc6a66d08b6665ba7e89e3427
Tyler <108450341+JW33R@users.noreply.github.com>
2025-09-22 13:52:39 -0400

    Update CW The Pattern.py

--- 7kyu/ThePattern/CW The Pattern.py
+++ 7kyu/ThePattern/CW The Pattern.py
@@ -1,3 +1,5 @@
+#The pattern function takes in 1 value n and checks to see if it is less than or equal to 0, and if it is, then it returns an empty string. Then whatever number the user inputs is taken and multiplied by the number they added to create a pattern
+#After that, it is added to a list and then returned with a new line indicator.
 def pattern(n: int) -> str:
     if n <= 0:
         return ""
@@ -7,3 +9,4 @@ def pattern(n: int) -> str:
     return "\n".join(result)
       
 print(pattern(5))
+
