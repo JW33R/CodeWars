734add68cc0aecf26ac9346d5fd2e2e279655ab3
Tyler <108450341+JW33R@users.noreply.github.com>
2025-09-22 13:38:28 -0400

    Update CW Diagonal Sum.py

--- 7kyu/DiagonalArray/CW Diagonal Sum.py
+++ 7kyu/DiagonalArray/CW Diagonal Sum.py
@@ -1,4 +1,5 @@
-
+#diagonalSum takes in an array of numbers. Then it uses the sum function to add the values together for however long the array is.
 def diagonalSum(array):
     return sum(array[i][i] for i in range(len(array)))
 print(diagonalSum(""))
+
