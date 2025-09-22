77ddda6f936851fe9494382e31173ba717c62bbd
Tyler <108450341+JW33R@users.noreply.github.com>
2025-09-22 13:42:08 -0400

    Update CW Interest.py

--- 7kyu/Interest/CW Interest.py
+++ 7kyu/Interest/CW Interest.py
@@ -1,4 +1,5 @@
-from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_DOWN
+#the interest function takes in 3 values. It multiplies and adds them to the regular var, then rounds and appends it to an array. Then the next for loop does the same, but doesn't add it to compound. It then returns the 
+#total
 def interest(p, r, n):
     total = []
     regular = p
@@ -16,4 +17,5 @@ def interest(p, r, n):
     compoundPrincipal = round(compoundPrincipal)
     total.append(int(compoundPrincipal))
     return total
+
 print(interest(303, .9, 47))
