64ce1838e6b589aaffa04440d63cdc721f5e5fb4
Tyler <108450341+JW33R@users.noreply.github.com>
2025-10-24 01:43:46 -0400

    Add files via upload

---
+++ 8kyu/CodeWarsTwiceAsOld/CW Twice As Old.py
@@ -0,0 +1,5 @@
+#This program finds the age where the father will be twice the age of his son
+def twiceAsOld(dadYearsOld, sonYearsOld):
+    return abs(dadYearsOld - sonYearsOld * 2)
+print(twiceAsOld(100, 61))
+    
